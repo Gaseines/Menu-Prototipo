f02fbe47e87d959c4e0ed2add2cbf60db1459479
Gaseines <gaseinesdzn@hotmail.com>
2025-08-16 12:25:33 -0300

    algumas ideias e ajustes

--- cardapio/src/App.js
+++ cardapio/src/App.js
@@ -4,6 +4,7 @@ import Capa from './pages/Capa';
 
 function App() {
   return (
+    
     <div className="App">
       <Capa />
     </div>
--- cardapio/src/pages/Capa.jsx
+++ cardapio/src/pages/Capa.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import logo from "../assets/images/teste.png"
-import styles from "./Capa.module.css"
+import React from "react";
+import logo from "../assets/images/teste.png";
+import styles from "./Capa.module.css";
 
 const Capa = () => {
   return (
     <div className={styles.all}>
-      <img className={styles.logo} src={logo} alt="logo" />
+      <div className={styles.container_img}>
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>
       <div className={styles.container_nav}>
-          <div className={`${styles.nav_1} ${styles.nav}`}>
-            <h1>Menu</h1>
-          </div>
-          <div className={`${styles.nav_2} ${styles.nav}`}>
-            <h1>Reserva</h1>
-          </div>
+        <div className={`${styles.nav_1} ${styles.nav}`}>
+          <h1 className={styles.option}>Menu</h1>
+        </div>
+        <div className={`${styles.nav_2} ${styles.nav}`}>
+          <h1 className={styles.option}>Reserva</h1>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Capa
+export default Capa;
